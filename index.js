0d33ebda9734063723c803e316b2a24b7dedd0cb
Dany Atanov <Dany Atanov>
2025-01-30 10:56:35 +0300

    [~] добавление товаров

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import Invoice from "./invoice.js";
 
- let invoice = new Invoice();
-/* let invoice = new Invoice({
-  number: "69",
-  date: "11 декабря",
-}); */
+//let invoice = new Invoice();
+let invoice = new Invoice({
+ /*  productList: [
+    {
+      number: "34344",
+      name: "Еще один товара",
+      quantity: "5",
+      price: "20122.50",
+    },
+  ], */
+});
 
 console.log(invoice);
 
 invoice.generate();
+//invoice.test();
