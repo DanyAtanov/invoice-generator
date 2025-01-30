4a40e71ad756547d726e8199c386d68fd7a8c64c
Dany Atanov <Dany Atanov>
2025-01-30 14:36:44 +0300

    [~] верстка и добавление актуальных реквизитов

--- example.js
+++ example.js
@@ -1,111 +1,246 @@
-"use strict";
-function ownKeys(a, b) {
-  var c = Object.keys(a);
-  if (Object.getOwnPropertySymbols) {
-    var d = Object.getOwnPropertySymbols(a);
-    b &&
-      (d = d.filter(function (b) {
-        return Object.getOwnPropertyDescriptor(a, b).enumerable;
-      })),
-      c.push.apply(c, d);
-  }
-  return c;
+function template(invoice) {
+  return `<table width="100%" style="font-family: Arial">
+<tr>
+  <td style="width: 68%; padding: 20px 0">
+    <div style="text-align: justify; font-size: 11pt">
+      Внимание! Оплата данного счета означает согласие с условиями поставки
+      товара. Счет действителен в течение 5(пяти) банковских дней, не считая
+      дня выписки счета. Уведомление об оплате обязательно, в противном случае
+      НЕ ГАРАНТИРУЕТСЯ наличие товара на складе. Товар отпускается по факту
+      прихода денег на р/с Поставщика, самовывозом, при наличии доверенности и
+      паспорта.
+    </div>
+  </td>
+  <td style="width: 32%; text-align: center; padding: 30px 0">
+    <img src="<!--Лого url-->" style="width: 70%" />
+  </td>
+</tr>
+</table>
+
+<table
+width="100%"
+border="2"
+style="border-collapse: collapse; width: 100%; font-family: Arial"
+cellpadding="2"
+cellspacing="2"
+>
+<tr style="">
+  <td colspan="2" rowspan="2" style="min-height: 13mm; width: 105mm">
+    <table
+      width="100%"
+      border="0"
+      cellpadding="0"
+      cellspacing="0"
+      style="height: 13mm"
+    >
+      <tr>
+        <td valign="top">
+          <div>СТ-ПЕТЕРБУРГСКИЙ филиал ПАО "ПРОМСВЯЗЬБАНК"<br />Г.</div>
+        </td>
+      </tr>
+      <tr>
+        <td valign="bottom" style="height: 3mm">
+          <div style="font-size: 10pt">Банк получателя</div>
+        </td>
+      </tr>
+    </table>
+  </td>
+  <td style="min-height: 7mm; height: auto; width: 25mm">
+    <div>БИK</div>
+  </td>
+  <td rowspan="2" style="vertical-align: top; width: 60mm">
+    <div style="height: 7mm; line-height: 7mm; vertical-align: middle">
+      044030555
+    </div>
+    <div>30101810000000000555</div>
+  </td>
+</tr>
+<tr>
+  <td style="width: 25mm">
+    <div>Сч. №</div>
+  </td>
+</tr>
+<tr>
+  <td style="min-height: 6mm; height: auto; width: 50mm">
+    <div>ИНН</div>
+  </td>
+  <td style="min-height: 6mm; height: auto; width: 55mm">
+    <div>КПП</div>
+  </td>
+  <td
+    rowspan="2"
+    style="min-height: 19mm; height: auto; vertical-align: top; width: 25mm"
+  >
+    <div>Сч. №</div>
+  </td>
+  <td
+    rowspan="2"
+    style="min-height: 19mm; height: auto; vertical-align: top; width: 60mm"
+  >
+    <div>40702810306000008712</div>
+  </td>
+</tr>
+<tr>
+  <td colspan="2" style="min-height: 13mm; height: auto">
+    <table
+      border="0"
+      cellpadding="0"
+      cellspacing="0"
+      style="height: 13mm; width: 105mm"
+    >
+      <tr>
+        <td valign="top">
+          <div>ООО ""</div>
+        </td>
+      </tr>
+      <tr>
+        <td valign="bottom" style="height: 3mm">
+          <div style="font-size: 10pt">Получатель</div>
+        </td>
+      </tr>
+    </table>
+  </td>
+</tr>
+</table>
+<br />
+
+<div
+style="
+  font-weight: bold;
+  font-size: 25pt;
+  padding-left: 5px;
+  font-family: Arial;
+"
+>
+Счет № ${invoice.number} от ${invoice.date}
+</div>
+<br />
+
+<div
+style="background-color: #000000; width: 100%; font-size: 1px; height: 2px"
+>
+&nbsp;
+</div>
+
+<table width="100%" style="font-family: Arial">
+<tr>
+  <td style="width: 30mm; vertical-align: top">
+    <div style="padding-left: 2px">Поставщик:</div>
+  </td>
+  <td>
+    <div style="font-weight: bold; padding-left: 2px">
+      ООО "" ИНН , КПП ,<br />
+      <span style="font-weight: normal"
+        >, Российская Федерация, г. , Невский пр-кт, д. лит. ,<br />
+        пом. , тел.: +7() , факс: +7()
+      </span>
+    </div>
+  </td>
+</tr>
+<tr>
+  <td style="width: 30mm; vertical-align: top">
+    <div style="padding-left: 2px">Покупатель:</div>
+  </td>
+  <td>
+    <div style="font-weight: bold; padding-left: 2px">
+      ИП , ИНН 7564644646, КПП 45465446456,<br /><span
+        style="font-weight: normal"
+        >213245, Российская Федерация, г. , пр-кт, д.151 лит. А,<br />
+        пом. , тел.: +7() , факс: +7()
+      </span>
+    </div>
+  </td>
+</tr>
+</table>
+
+<table
+border="2"
+width="100%"
+cellpadding="2"
+cellspacing="2"
+style="border-collapse: collapse; width: 100%; font-family: Arial"
+>
+<thead>
+  <tr>
+    <th style="width: 13mm">№</th>
+    <th style="width: 17mm">Артикул</th>
+    <th>Товары (работы, услуги)</th>
+    <th style="width: 20mm">Кол-во</th>
+    <th style="width: 17mm">Ед.</th>
+    <th style="width: 27mm">Цена</th>
+    <th style="width: 27mm">Сумма</th>
+  </tr>
+</thead>
+<tbody>
+ ${templateCart(invoice)}
+</tbody>
+</table>
+
+<table
+style="font-family: Arial"
+border="0"
+width="100%"
+cellpadding="1"
+cellspacing="1"
+>
+<tr>
+  <td></td>
+  <td style="width: 27mm; font-weight: bold; text-align: right">Итого:</td>
+  <td style="width: 27mm; font-weight: bold; text-align: center">0.00</td>
+</tr>
+<tr>
+  <td></td>
+  <td style="width: 27mm; font-weight: bold; text-align: right">
+    Итого НДС:
+  </td>
+  <td style="width: 27mm; font-weight: bold; text-align: center">0.00</td>
+</tr>
+<tr>
+  <td></td>
+  <td style="width: 37mm; font-weight: bold; text-align: right">
+    Всего к оплате:
+  </td>
+  <td style="width: 27mm; font-weight: bold; text-align: center">0.00</td>
+</tr>
+</table>
+
+<br />
+<div style="font-family: Arial">
+Всего наименований 0 на сумму 0.00 рублей.<br />
+Ноль рублей 00 копеек
+</div>
+<br /><br />
+<div
+style="background-color: #000000; width: 100%; font-size: 1px; height: 2px"
+>
+&nbsp;
+</div>
+<br />
+<div style="font-family: Arial; font-size: 10pt">
+1. Счет действителен в течении 5 (пяти) банковских дней, не считая дня выписки
+счета. В случае нарушения срока оплаты сохранение цены на товар и наличие
+товара на складе НЕ ГАРАНТИРУЕТСЯ.<br />
+2. Оплата данного счета означает согласие с условиями изложенными в п.1
+</div>
+<br /><br />
+<div
+style="
+  background: url('<!--url печати в png сюда-->');
+  background-repeat: no-repeat;
+  padding: 30px 10px;
+  width: 400px;
+  height: 250px;
+"
+>
+<div>Руководитель ______________________</div>
+<br />
+<br /><br />
+
+<div>Главный бухгалтер ______________________</div>
+<br />
+
+<div style="width: 85mm; text-align: center">М.П.</div>
+<br />
+</div>
+`;
 }
-function _objectSpread(a) {
-  for (var b, c = 1; c < arguments.length; c++)
-    (b = null == arguments[c] ? {} : arguments[c]),
-      c % 2
-        ? ownKeys(Object(b), !0).forEach(function (c) {
-            _defineProperty(a, c, b[c]);
-          })
-        : Object.getOwnPropertyDescriptors
-        ? Object.defineProperties(a, Object.getOwnPropertyDescriptors(b))
-        : ownKeys(Object(b)).forEach(function (c) {
-            Object.defineProperty(a, c, Object.getOwnPropertyDescriptor(b, c));
-          });
-  return a;
-}
-function _defineProperty(a, b, c) {
-  return (
-    b in a
-      ? Object.defineProperty(a, b, {
-          value: c,
-          enumerable: !0,
-          configurable: !0,
-          writable: !0,
-        })
-      : (a[b] = c),
-    a
-  );
-}
-const defaults = { threshold: 50, passive: !1 };
-class Xwiper {
-  constructor(a, b = {}) {
-    (this.options = _objectSpread(_objectSpread({}, defaults), b)),
-      (this.element = null),
-      (this.touchStartX = 0),
-      (this.touchStartY = 0),
-      (this.touchEndX = 0),
-      (this.touchEndY = 0),
-      (this.onSwipeLeftAgent = null),
-      (this.onSwipeRightAgent = null),
-      (this.onSwipeUpAgent = null),
-      (this.onSwipeDownAgent = null),
-      (this.onTapAgent = null),
-      (this.onTouchStart = this.onTouchStart.bind(this)),
-      (this.onTouchEnd = this.onTouchEnd.bind(this)),
-      (this.onSwipeLeft = this.onSwipeLeft.bind(this)),
-      (this.onSwipeRight = this.onSwipeRight.bind(this)),
-      (this.onSwipeUp = this.onSwipeUp.bind(this)),
-      (this.onSwipeDown = this.onSwipeDown.bind(this)),
-      (this.onTap = this.onTap.bind(this)),
-      (this.destroy = this.destroy.bind(this)),
-      (this.handleGesture = this.handleGesture.bind(this));
-    let c = !!this.options.passive && { passive: !0 };
-    (this.element = a instanceof EventTarget ? a : document.querySelector(a)),
-      this.element.addEventListener("touchstart", this.onTouchStart, c),
-      this.element.addEventListener("touchend", this.onTouchEnd, c);
-  }
-  onTouchStart(a) {
-    (this.touchStartX = a.changedTouches[0].screenX),
-      (this.touchStartY = a.changedTouches[0].screenY);
-  }
-  onTouchEnd(a) {
-    (this.touchEndX = a.changedTouches[0].screenX),
-      (this.touchEndY = a.changedTouches[0].screenY),
-      this.handleGesture();
-  }
-  onSwipeLeft(a) {
-    this.onSwipeLeftAgent = a;
-  }
-  onSwipeRight(a) {
-    this.onSwipeRightAgent = a;
-  }
-  onSwipeUp(a) {
-    this.onSwipeUpAgent = a;
-  }
-  onSwipeDown(a) {
-    this.onSwipeDownAgent = a;
-  }
-  onTap(a) {
-    this.onTapAgent = a;
-  }
-  destroy() {
-    this.element.removeEventListener("touchstart", this.onTouchStart),
-      this.element.removeEventListener("touchend", this.onTouchEnd);
-  }
-  handleGesture() {
-    return this.touchEndX + this.options.threshold <= this.touchStartX
-      ? (this.onSwipeLeftAgent && this.onSwipeLeftAgent(), "swiped left")
-      : this.touchEndX - this.options.threshold >= this.touchStartX
-      ? (this.onSwipeRightAgent && this.onSwipeRightAgent(), "swiped right")
-      : this.touchEndY + this.options.threshold <= this.touchStartY
-      ? (this.onSwipeUpAgent && this.onSwipeUpAgent(), "swiped up")
-      : this.touchEndY - this.options.threshold >= this.touchStartY
-      ? (this.onSwipeDownAgent && this.onSwipeDownAgent(), "swiped down")
-      : this.touchEndY === this.touchStartY
-      ? (this.onTapAgent && this.onTapAgent(), "tap")
-      : void 0;
-  }
-}
-export default Xwiper;
--- invoice.js
+++ invoice.js
@@ -2,248 +2,239 @@ let Invoice = (() => {
   "use strict";
 
   function template(invoice) {
-    return `<table width="100%" style="font-family: Arial">
-  <tr>
-    <td style="width: 68%; padding: 20px 0">
-      <div style="text-align: justify; font-size: 11pt">
-        Внимание! Оплата данного счета означает согласие с условиями поставки
-        товара. Счет действителен в течение 5(пяти) банковских дней, не считая
-        дня выписки счета. Уведомление об оплате обязательно, в противном случае
-        НЕ ГАРАНТИРУЕТСЯ наличие товара на складе. Товар отпускается по факту
-        прихода денег на р/с Поставщика, самовывозом, при наличии доверенности и
-        паспорта.
-      </div>
-    </td>
-    <td style="width: 32%; text-align: center; padding: 30px 0">
-      <img src="<!--Лого url-->" style="width: 70%" />
-    </td>
-  </tr>
-</table>
-
-<table
-  width="100%"
-  border="2"
-  style="border-collapse: collapse; width: 100%; font-family: Arial"
-  cellpadding="2"
-  cellspacing="2"
->
-  <tr style="">
-    <td colspan="2" rowspan="2" style="min-height: 13mm; width: 105mm">
-      <table
-        width="100%"
-        border="0"
-        cellpadding="0"
-        cellspacing="0"
-        style="height: 13mm"
-      >
-        <tr>
-          <td valign="top">
-            <div>СТ-ПЕТЕРБУРГСКИЙ филиал ПАО "ПРОМСВЯЗЬБАНК"<br />Г.</div>
-          </td>
-        </tr>
-        <tr>
-          <td valign="bottom" style="height: 3mm">
-            <div style="font-size: 10pt">Банк получателя</div>
-          </td>
-        </tr>
-      </table>
-    </td>
-    <td style="min-height: 7mm; height: auto; width: 25mm">
-      <div>БИK</div>
-    </td>
-    <td rowspan="2" style="vertical-align: top; width: 60mm">
-      <div style="height: 7mm; line-height: 7mm; vertical-align: middle">
-        044030555
-      </div>
-      <div>30101810000000000555</div>
-    </td>
-  </tr>
-  <tr>
-    <td style="width: 25mm">
-      <div>Сч. №</div>
-    </td>
-  </tr>
-  <tr>
-    <td style="min-height: 6mm; height: auto; width: 50mm">
-      <div>ИНН</div>
-    </td>
-    <td style="min-height: 6mm; height: auto; width: 55mm">
-      <div>КПП</div>
-    </td>
-    <td
-      rowspan="2"
-      style="min-height: 19mm; height: auto; vertical-align: top; width: 25mm"
-    >
-      <div>Сч. №</div>
-    </td>
-    <td
-      rowspan="2"
-      style="min-height: 19mm; height: auto; vertical-align: top; width: 60mm"
-    >
-      <div>40702810306000008712</div>
-    </td>
-  </tr>
-  <tr>
-    <td colspan="2" style="min-height: 13mm; height: auto">
-      <table
-        border="0"
-        cellpadding="0"
-        cellspacing="0"
-        style="height: 13mm; width: 105mm"
-      >
-        <tr>
-          <td valign="top">
-            <div>ООО ""</div>
-          </td>
-        </tr>
-        <tr>
-          <td valign="bottom" style="height: 3mm">
-            <div style="font-size: 10pt">Получатель</div>
-          </td>
-        </tr>
-      </table>
-    </td>
-  </tr>
-</table>
-<br />
-
-<div
-  style="
-    font-weight: bold;
-    font-size: 25pt;
-    padding-left: 5px;
-    font-family: Arial;
-  "
->
-  Счет № ${invoice.number} от ${invoice.date}
-</div>
-<br />
-
-<div
-  style="background-color: #000000; width: 100%; font-size: 1px; height: 2px"
->
-  &nbsp;
-</div>
-
-<table width="100%" style="font-family: Arial">
-  <tr>
-    <td style="width: 30mm; vertical-align: top">
-      <div style="padding-left: 2px">Поставщик:</div>
-    </td>
-    <td>
-      <div style="font-weight: bold; padding-left: 2px">
-        ООО "" ИНН , КПП ,<br />
-        <span style="font-weight: normal"
-          >, Российская Федерация, г. , Невский пр-кт, д. лит. ,<br />
-          пом. , тел.: +7() , факс: +7()
-        </span>
-      </div>
-    </td>
-  </tr>
-  <tr>
-    <td style="width: 30mm; vertical-align: top">
-      <div style="padding-left: 2px">Покупатель:</div>
-    </td>
-    <td>
-      <div style="font-weight: bold; padding-left: 2px">
-        ИП , ИНН 7564644646, КПП 45465446456,<br /><span
-          style="font-weight: normal"
-          >213245, Российская Федерация, г. , пр-кт, д.151 лит. А,<br />
-          пом. , тел.: +7() , факс: +7()
-        </span>
-      </div>
-    </td>
-  </tr>
-</table>
-
-<table
-  border="2"
-  width="100%"
-  cellpadding="2"
-  cellspacing="2"
-  style="border-collapse: collapse; width: 100%; font-family: Arial"
->
-  <thead>
+    return `<div style="font-size: 10pt; font-family: Arial">
+  <table width="100%">
     <tr>
-      <th style="width: 13mm">№</th>
-      <th style="width: 17mm">Артикул</th>
-      <th>Товары (работы, услуги)</th>
-      <th style="width: 20mm">Кол-во</th>
-      <th style="width: 17mm">Ед.</th>
-      <th style="width: 27mm">Цена</th>
-      <th style="width: 27mm">Сумма</th>
+      <td style="width: 68%; padding: 0 0 10px">
+        <div style="text-align: justify; font-size: 6pt">
+          Внимание! Оплата данного счета означает согласие с условиями поставки
+          товара. Счет действителен в течение 5(пяти) банковских дней, не считая
+          дня выписки счета. Уведомление об оплате обязательно, в противном
+          случае НЕ ГАРАНТИРУЕТСЯ наличие товара на складе. Товар отпускается по
+          факту прихода денег на р/с Поставщика, самовывозом, при наличии
+          доверенности и паспорта.
+        </div>
+      </td>
+      <td style="width: 32%; text-align: center; padding: 0 0 10px">
+        <img src="<!--Лого url-->" style="width: 70%" />
+      </td>
     </tr>
-  </thead>
-  <tbody>
-   ${templateCart(invoice)}
-  </tbody>
-</table>
+  </table>
 
-<table
-  style="font-family: Arial"
-  border="0"
-  width="100%"
-  cellpadding="1"
-  cellspacing="1"
->
-  <tr>
-    <td></td>
-    <td style="width: 27mm; font-weight: bold; text-align: right">Итого:</td>
-    <td style="width: 27mm; font-weight: bold; text-align: center">0.00</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td style="width: 27mm; font-weight: bold; text-align: right">
-      Итого НДС:
-    </td>
-    <td style="width: 27mm; font-weight: bold; text-align: center">0.00</td>
-  </tr>
-  <tr>
-    <td></td>
-    <td style="width: 37mm; font-weight: bold; text-align: right">
-      Всего к оплате:
-    </td>
-    <td style="width: 27mm; font-weight: bold; text-align: center">0.00</td>
-  </tr>
-</table>
+  <table
+    width="100%"
+    border="1"
+    style="border-collapse: collapse; width: 100%; font-size: 10pt"
+    cellpadding="2"
+    cellspacing="2"
+  >
+    <tr>
+      <td colspan="2" rowspan="2" style="min-height: 13mm; width: 105mm">
+        <table
+          width="100%"
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          style="height: 13mm"
+        >
+          <tr>
+            <td valign="top">
+              <div style="font-size: 10pt">
+                Ф-Л "СЕВЕРНАЯ СТОЛИЦА" АО "РАЙФФАЙЗЕНБАНК" г. Санкт-Петербург
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td valign="bottom" style="height: 3mm">
+              <div style="font-size: 9pt">Банк получателя</div>
+            </td>
+          </tr>
+        </table>
+      </td>
+      <td style="min-height: 7mm; height: auto; width: 25mm">
+        <div style="font-size: 9pt">БИK</div>
+      </td>
+      <td rowspan="2" style="vertical-align: top; width: 60mm">
+        <div style="height: 7mm; line-height: 7mm; vertical-align: middle">
+          044030723
+        </div>
+        <div>30101810100000000723</div>
+      </td>
+    </tr>
+    <tr>
+      <td style="width: 25mm">
+        <div>Сч. №</div>
+      </td>
+    </tr>
+    <tr>
+      <td style="min-height: 6mm; height: auto; width: 50mm">
+        <div>ИНН 7816189554</div>
+      </td>
+      <td style="min-height: 6mm; height: auto; width: 55mm">
+        <div>КПП 781101001</div>
+      </td>
+      <td
+        rowspan="2"
+        style="min-height: 19mm; height: auto; vertical-align: top; width: 25mm"
+      >
+        <div>Сч. №</div>
+      </td>
+      <td
+        rowspan="2"
+        style="min-height: 19mm; height: auto; vertical-align: top; width: 60mm"
+      >
+        <div>40702810603000079889</div>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" style="min-height: 13mm; height: auto">
+        <table
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          style="height: 13mm; width: 105mm"
+        >
+          <tr>
+            <td valign="top">
+              <div style="font-size: 10pt">
+                Общество с ограниченной ответственностью Торговый Дом "АВТОграф"
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td valign="bottom" style="height: 3mm">
+              <div style="font-size: 9pt">Получатель</div>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
 
-<br />
-<div style="font-family: Arial">
-  Всего наименований 0 на сумму 0.00 рублей.<br />
-  Ноль рублей 00 копеек
-</div>
-<br /><br />
-<div
-  style="background-color: #000000; width: 100%; font-size: 1px; height: 2px"
->
-  &nbsp;
-</div>
-<br />
-<div style="font-family: Arial; font-size: 10pt">
-  1. Счет действителен в течении 5 (пяти) банковских дней, не считая дня выписки
-  счета. В случае нарушения срока оплаты сохранение цены на товар и наличие
-  товара на складе НЕ ГАРАНТИРУЕТСЯ.<br />
-  2. Оплата данного счета означает согласие с условиями изложенными в п.1
-</div>
-<br /><br />
-<div
-  style="
-    background: url('<!--url печати в png сюда-->');
-    background-repeat: no-repeat;
-    padding: 30px 10px;
-    width: 400px;
-    height: 250px;
-  "
->
-  <div>Руководитель ______________________</div>
+  <div style="font-weight: bold; font-size: 14pt; padding-left: 5px; padding-top: 10px;">
+    Счет № ${invoice.number} от ${invoice.date}
+  </div>
   <br />
+
+  <div
+    style="background-color: #000000; width: 100%; font-size: 1px; height: 2px"
+  >
+    &nbsp;
+  </div>
+
+  <table width="100%" style="font-size: 9pt">
+    <tr>
+      <td style="width: 30mm; vertical-align: top">
+        <div style="padding-left: 2px">Поставщик:</div>
+      </td>
+      <td>
+        <div style="font-weight: bold; padding-left: 2px">
+          Общество с ограниченной ответственностью Торговый Дом "АВТОграф", ИНН
+          7816189554, КПП 781101001, 192019, Санкт-Петербург г, Седова ул, дом
+          11, литера Б, помещение 19-Н, тел.: (812) 334-39-96
+        </div>
+      </td>
+    </tr>
+    <tr></tr>
+    <tr></tr>
+    <tr></tr>
+    <tr></tr>
+    <tr>
+      <td style="width: 30mm; vertical-align: top">
+        <div style="padding-left: 2px">Покупатель:</div>
+      </td>
+      <td>
+        <div style="font-weight: bold; padding-left: 2px">
+          ИП Илон Масков , ИНН 660000000000, 90210, Лос-Анджелес, Беверли-Хилз,
+          спросить Илона
+        </div>
+      </td>
+    </tr>
+  </table>
+
+  <table
+    border="2"
+    width="100%"
+    cellpadding="2"
+    cellspacing="2"
+    style="border-collapse: collapse; width: 100%; font-size: 10pt"
+  >
+    <thead>
+      <tr>
+        <th style="width: 13mm">№</th>
+        <th style="width: 27mm">Артикул</th>
+        <th>Товары (работы, услуги)</th>
+        <th style="width: 20mm">Кол-во</th>
+        <th style="width: 17mm">Ед.</th>
+        <th style="width: 35mm">Цена</th>
+        <th style="width: 35mm">Сумма</th>
+      </tr>
+    </thead>
+    <tbody>
+    ${templateCart(invoice)}
+    </tbody>
+  </table>
+
+  <table
+    border="0"
+    width="100%"
+    cellpadding="1"
+    cellspacing="1"
+    style="font-size: 10pt; padding-top: 5px"
+  >
+    <tr>
+      <td></td>
+      <td style="width: 27mm; font-weight: bold; text-align: right">Итого:</td>
+      <td style="width: 35mm; font-weight: bold; text-align: center">0.00</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td style="width: 27mm; font-weight: bold; text-align: right">
+        В т.ч. НДС:
+      </td>
+      <td style="width: 35mm; font-weight: bold; text-align: center">0.00</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td style="width: 37mm; font-weight: bold; text-align: right">
+        Итого с НДС:
+      </td>
+      <td style="width: 35mm; font-weight: bold; text-align: center">0.00</td>
+    </tr>
+  </table>
+
+  <br />
+  <div style="font-size: 10pt">
+    Всего наименований 0 на сумму 0.00 рублей.<br />
+    <b>Ноль рублей 00 копеек</b>
+  </div>
   <br /><br />
-
-  <div>Главный бухгалтер ______________________</div>
+  <div
+    style="background-color: #000000; width: 100%; font-size: 1px; height: 2px"
+  >
+    &nbsp;
+  </div>
   <br />
-
-  <div style="width: 85mm; text-align: center">М.П.</div>
   <br />
+  <table
+    width="100%"
+    style="border-collapse: collapse; width: 100%; font-size: 10pt"
+    cellpadding="2"
+    cellspacing="2"
+  >
+    <tr>
+      <td style="width: 20mm"><b>Руководител</b>ь</td>
+      <td style="width: 30mm; border-bottom: 1px solid #000"></td>
+      <td>Громов И.С.</td>
+    </tr>
+    <tr></tr>
+    <tr></tr>
+    <tr>
+      <td style="width: 20mm"><b>Бухгалтер</b></td>
+      <td style="width: 30mm; border-bottom: 1px solid #000"></td>
+      <td>Майорова С. Р.</td>
+    </tr>
+  </table>
 </div>
 `;
   }
